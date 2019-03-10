9bd995a4e0b6714667343cdeabb8416bf50526b1
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-03-10 10:28:39 +0530

    Configured cloud config and eureka

--- src/main/java/com/atuldwivedi/quickcart/component/order/rest/OrderController.java
+++ src/main/java/com/atuldwivedi/quickcart/component/order/rest/OrderController.java
@@ -1,28 +1,29 @@
 package com.atuldwivedi.quickcart.component.order.rest;
 
-import java.util.Arrays;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
+import com.atuldwivedi.quickcart.component.order.domain.Order;
+import com.atuldwivedi.quickcart.component.order.mock.OrderData;
+import com.atuldwivedi.quickcart.component.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-import com.atuldwivedi.quickcart.component.order.domain.Order;
-import com.atuldwivedi.quickcart.component.order.mock.OrderData;
-import com.atuldwivedi.quickcart.component.order.service.OrderService;
+import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+	@Autowired
+	DiscoveryClient client;
 
 	@Autowired
 	private OrderData orderData;
@@ -38,7 +39,7 @@ public class OrderController {
 	@RequestMapping(value = "/orders", method = RequestMethod.GET)
 	public ResponseEntity<List<Order>> getOrders() {
 		List<Order> orders = orderService.getOrders();
-		MultiValueMap<String, String> headers = new HttpHeaders(); 
+		MultiValueMap<String, String> headers = new HttpHeaders();
 		headers.put("message", Arrays.asList("Success"));
 		return new ResponseEntity(orders, headers, HttpStatus.OK);
 	}
@@ -61,6 +62,28 @@ public class OrderController {
 	@RequestMapping(value = "/order", method = RequestMethod.PUT)
 	public Order updateOrder(@RequestBody Order order) {
 		return orderService.updateOrder(order);
+	}
+
+
+	@GetMapping("/options")
+	public @ResponseBody
+	String getOrderOptions() {
+		return
+				getWord("component-parcel", "/rest/v1/parcels/modes") + " "
+						+ getWord("component-payment", "/rest/v1/payments/modes") + " ";
+	}
+
+	public String getWord(String service, String path) {
+		List<ServiceInstance> list = client.getInstances(service);
+		if (list != null && list.size() > 0) {
+			URI uri = list.get(0).getUri();
+			if (uri != null) {
+				String completePath = "http://"+uri.getHost()+":"+uri.getPort()+path;
+				System.out.println("Complete path------------- "+completePath);
+				return (new RestTemplate()).getForObject(completePath, String.class);
+			}
+		}
+		return null;
 	}
 
 }
